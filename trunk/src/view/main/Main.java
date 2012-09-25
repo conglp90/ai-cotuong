c5a360ed868a0abd34d863d97cca6b28e06b882a
heroandtn3@gmail.com <heroandtn3@gmail.com@57533ae6-525d-af93-b7ef-82e61300d119>
2012-09-25 16:40:46 +0000

    Test conflict

--- trunk/src/view/main/Main.java
+++ trunk/src/view/main/Main.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 import javax.swing.*;
 	// dong nay viet tren may cua Sang
-	// them mot dong nua tren may cua Sng, dong nay lai duoc sua tiep
+	// them mot dong nua tren may cua Sang, dong nay lai duoc sua tiep
 public class Main{
 	public static int WIDTH=900;
 	public static int HEIGHT=720;
