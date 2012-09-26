02d720dd8403340d000435728efc6691ca224dab
heroandtn3@gmail.com <heroandtn3@gmail.com@57533ae6-525d-af93-b7ef-82e61300d119>
2012-09-26 11:13:04 +0000

    Tạo package control để xử lý các luồng điều khiển. + Di chuyển file TuyChon.java trong view sang control

---
+++ trunk/src/control/TuyChon.java
@@ -0,0 +1,41 @@
+package view;
+
+/* 
+   
+ */
+
+
+class TuyChon {
+
+    public boolean sound;
+
+    public final void undo() {
+    
+    }
+
+    public final void redo() {
+    
+    }
+
+    public final void newGame() {
+    
+    }
+
+    public final void loadGame() {
+    
+    }
+
+    public final void saveGame() {
+    
+    }
+
+    public final void exit() {
+    
+    }
+
+    public final void hint() {
+    
+    }
+
+
+}
