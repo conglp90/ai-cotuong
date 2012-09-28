a9d6484970deb8d499fb54033a933346aaba2237
heroandtn3 <heroandtn3@gmail.com>
2012-09-28 08:02:41 +0000

    + Thêm tiếp các lớp trong pakage chess

---
+++ src/model/chess/Cannon.java
@@ -0,0 +1,22 @@
+package model.chess;
+
+import java.awt.Image;
+
+import model.ChessPosition;
+
+public class Cannon extends Chess {
+
+	public Cannon(Image shape, int row, int col) {
+		super(shape, row, col);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ChessPosition[] getPosCanMove(ChessPosition current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
