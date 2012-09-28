a3ec7f25db907e1f3b59e2c7a07d2c3762a27741
khanhoait.bka@gmail.com <khanhoait.bka@gmail.com@57533ae6-525d-af93-b7ef-82e61300d119>
2012-09-28 16:52:54 +0000

    hoa update

--- trunk/src/model/Constant.java
+++ trunk/src/model/Constant.java
@@ -2,13 +2,18 @@ package model;
 
 public class Constant {
 	public final static int MAIN_WIDTH=900;
-	public final static int MAIN_HEIGHT=720;
+	public final static int MAIN_HEIGHT=700;
 	public final static int LOCATEX=250;
 	public final static int LOCATEY=5;
-	public final static int UNDO_X=800;
+	public final static int UNDO_X=800;//toa do Undo
 	public final static int UNDO_Y=300;
-	public final static int SAVE_X=800;
+	public final static int SAVE_X=800;//toa do Redo
 	public final static int SAVE_Y=200;
-	public final static int BOARD_X=200;
-	public final static int BOARD_Y=0;
+	public final static int BOARD_X=150;//vi tri ban co X
+	public final static int BOARD_Y=0;//vi tri ban co Y
+	public static final int BOARD_WIDTH=600;//chieu rong ban co
+	public static final int BOARD_HEIGHT=700-30;//chieu cao ban co
+	public static final int OX=165;//tam toa do OX
+	public static final int OY=15;
+	public static final int length=67;//khoang cach giua 2 o ban co
 }
