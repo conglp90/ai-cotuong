5f9e75d1f301b95afe9ef9f3920bd88c2668503e
heroandtn3@gmail.com <heroandtn3@gmail.com@57533ae6-525d-af93-b7ef-82e61300d119>
2012-09-28 08:00:05 +0000

    + Tạo package chess và di chuyển lớp Chess.java vào trong package chess + Tạo các lớp trong package chess + Đổi tên lớp Position.java thành ChessPosition.java + Code thêm cho lớp Match

---
+++ trunk/src/model/chess/Chess.java
@@ -0,0 +1,57 @@
+package model.chess;
+
+import java.awt.Image;
+
+import model.ChessPosition;
+
+public abstract class Chess {
+
+	private int row, col; //toa do 9 x 10
+	Image shape;
+	boolean died =false;
+	
+	public Chess(Image shape, int row, int col) {
+		this.row = row;
+		this.col = col;
+		this.shape = shape;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getCol() {
+		return this.col;
+	}
+	
+	public void setCol(int col) {
+		this.col = col;
+	}
+	
+	public int getX() {
+		return row; //tinh sau
+	}
+	
+	public int getY() {
+		return col; //tinh sau
+	}
+	
+	public Image getShape() {
+		return this.shape;
+	}
+	
+	public boolean isDied() {
+		return this.died;
+	}
+	
+	public void setDied(boolean status) {
+		this.died = status;
+	}
+	
+	// tra ve mang chua vi tri 
+	public abstract ChessPosition[] getPosCanMove(ChessPosition current);
+}
