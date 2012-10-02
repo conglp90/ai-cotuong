3316cf679923c3957a506574ebf431afa8da71f9
khanhoait.bka@gmail.com <khanhoait.bka@gmail.com@57533ae6-525d-af93-b7ef-82e61300d119>
2012-10-02 07:14:10 +0000

    Hoa up

--- trunk/src/view/ChessBoardPanel.java
+++ trunk/src/view/ChessBoardPanel.java
@@ -2,25 +2,34 @@ package view;
 
 import javax.swing.*;
 
+import model.ChessPosition;
 import model.Constant;
 import model.Match;
 import model.chess.*;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChessBoardPanel extends JPanel {
+public class ChessBoardPanel extends JPanel implements MouseMotionListener,MouseListener{
 	/**
 	 * 
 	 */
+	private int x1=-1,y1=-1;
 	private static final long serialVersionUID = 1L;
 	/**
 	 * 
 	 */
-	// private static final long serialVersionUID = 1976248615976616148L;
 	Match match = new Match();
-
+	Rook rook;
+	List <ChessPosition> pos = new ArrayList<ChessPosition>();
+	
 	public ChessBoardPanel() {
 		loadImage();
+		addMouseListener(this);
 	}
 
 	public void loadImage() {
@@ -41,17 +50,80 @@ public class ChessBoardPanel extends JPanel {
 		for (int i = 0; i < 10; i++)
 			for (int j = 0; j < 9; j++) {
 				int x = match.tablePos[i][j];
+				if (j==x1&&i==y1){
+					g.drawImage(match.imgSelect,
+							Constant.OX+j*Constant.length,
+							Constant.OY+i*Constant.length,42,42,null);
+				}
 				if (x > 0) {
 					g.drawImage(match.redChess[x].getShape(), 
 								Constant.OX + j*Constant.length, 
 								Constant.OY + i*Constant.length, 
 								42, 42, null);
-				} else if (x < 0)
+				} else 
+					if (x < 0)
 					g.drawImage(match.blackChess[-x].getShape(),
 								Constant.OX	+ j*Constant.length,
 								Constant.OY + i*Constant.length,
 								42, 42, null);
+				
 			}
+		repaint();	
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int x=(e.getX()-Constant.OX)/Constant.length;
+		int y=(e.getY()-Constant.OY)/Constant.length;
+		if (x>=0&&x<9&& y>=0&&y<10&&match.tablePos[y][x]!=0){
+			x1=x;
+			y1=y;
+		}
+		if (x>=0&&x<9&& y>=0&&y<10&&match.tablePos[y][x]==6){
+			rook=new Rook(Constant.CHESS_DIR + "/xedo.png",y,x);
+			pos=rook.getPosCanMove(null,match);
+		}
+		if (x>=0&&x<9&& y>=0&&y<10&&match.tablePos[y][x]==0&&x1>=0&&y1>=0){
+			match.tablePos[y][x]=match.tablePos[y1][x1];
+			match.tablePos[y1][x1]=0;
+			x1=-1;
+			y1=-1;
+		}	
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
